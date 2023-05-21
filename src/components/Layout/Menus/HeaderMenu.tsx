f147b9b572f60f6047a77a11b5bb347931b196ef
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2023-05-21 16:18:28 -0500

    feat(menus): add header and footer menus

--- src/components/Layout/Menus/HeaderMenu.tsx
+++ src/components/Layout/Menus/HeaderMenu.tsx
@@ -4,21 +4,38 @@ import { Link } from "gatsby"
 import useMenu from "../../../hooks/useMenu"
 
 export default function HeaderMenu() {
-  const menuItems = useMenu({ slug: "header-menu" })
+  // This will grab your header menu, if it is named Header Menu.
+  const { menu, siteInfo } = useMenu({ slug: "header-menu" })
 
   return (
     <>
-      {menuItems &&
-        menuItems.map((item: any) => {
-          return (
-            <Link
-              to={item.connectedNode.node.uri}
-              style={{ marginRight: "15px" }}
-            >
-              {item.label}
-            </Link>
-          )
-        })}
+      <div
+        className={
+          "mx-auto mb-12 px-4 sm:px-6 lg:px-8 w-full break-normal bg-red-100	border border-solid border-red-200 rounded-md	p-5"
+        }
+      >
+        <div className="rounded-md bg-red-50 p-4">
+          <div className="flex w-full">
+            <div className="ml-3 max-w-full	w-full">
+              <div className="flex justify-between text-sm text-red-700">
+                <h2 className="not-prose text-sm font-medium text-red-800">
+                  {siteInfo.generalSettings.title}
+                </h2>
+                <div>
+                  {menu &&
+                    menu.map((item: any) => {
+                      return (
+                        <Link to={item.uri} style={{ marginRight: "15px" }}>
+                          {item.label}
+                        </Link>
+                      )
+                    })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/hooks/useMenu.ts
+++ src/hooks/useMenu.ts
@@ -6,7 +6,8 @@ interface UseMenuItemsProps {
 }
 
 const useMenu = ({ location, slug }: UseMenuItemsProps) => {
-  const {
+  const { 
+    allWp,
     allWpMenu: { nodes },
   } = useStaticQuery(graphql`
     {
@@ -16,11 +17,7 @@ const useMenu = ({ location, slug }: UseMenuItemsProps) => {
           locations
           menuItems {
             nodes {
-              connectedNode {
-                node {
-                  uri
-                }
-              }
+              uri
               url
               label
               target
@@ -30,19 +27,30 @@ const useMenu = ({ location, slug }: UseMenuItemsProps) => {
           }
         }
       }
+      allWp {
+        nodes {
+          generalSettings {
+            title
+            url
+            description
+            dateFormat
+          }
+        }
+      }
     }
   `)
 
   if (location) {
     const menu =
       nodes && nodes.find((node: any) => node.locations.includes(location))
-    return menu?.menuItems?.nodes
+    return {menu: menu?.menuItems?.nodes, siteInfo: allWp.nodes[0]}
   }
 
   if (slug) {
     const menu = nodes.find((node: any) => node.slug === slug)
-    return menu?.menuItems?.nodes
+    return {menu: menu?.menuItems?.nodes, siteInfo: allWp.nodes[0]}
   }
+  return {menu: [], siteInfo: allWp.nodes[0]}
 }
 
 export default useMenu
